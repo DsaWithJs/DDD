6d67e21f66d914441025d87ef72758e5c03b2ce3
DsaWithJs <nagaraju.jnd@gmail.com>
2023-11-18 12:29:34 +0530

    ss

--- examples/2.sort array of objects based on another array.ts
+++ examples/2.sort array of objects based on another array.ts
@@ -9,6 +9,7 @@ const objects: Person[] = [
   { name: "Alice", age: 30 },
   { name: "Bob", age: 25 },
   { name: "Eve", age: 35 },
+  { name: "Charlie", age: 28 }, // Not in the order array
 ];
 
 // Array of strings defining the order
@@ -16,7 +17,15 @@ const order: string[] = ["Eve", "Alice", "Bob"];
 
 // Function to sort the objects array based on the order array
 function sortByOrder(objects: Person[], order: string[]): Person[] {
-  return objects.sort((a, b) => order.indexOf(a.name) - order.indexOf(b.name));
+  return objects.sort((a, b) => {
+    const indexA = order.indexOf(a.name);
+    const indexB = order.indexOf(b.name);
+
+    if (indexA === -1) return 1; // Place 'a' at the end if not found
+    if (indexB === -1) return -1; // Place 'b' at the end if not found
+
+    return indexA - indexB;
+  });
 }
 
 // Call the function and log the result
